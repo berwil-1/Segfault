29aab3d7b8e07bbc07aa3dd051f486524675b739
berwil-1 <berghwilliam@gmail.com>
2025-07-28 18:49:19 +0200

    Performance fix PGN

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ CXX_FLAGS="-O3 -march=native -fno-rtti"
 LD_FLAGS="-flto"
 
 # Configure the build with Clang and optimizations
-CC=gcc CXX=g++ cmake -B build -DCMAKE_BUILD_TYPE=Debug \
+CC=gcc CXX=g++ cmake -B build -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_FLAGS_RELEASE="$CXX_FLAGS" \
     -DCMAKE_EXE_LINKER_FLAGS_RELEASE="$LD_FLAGS" \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
--- train/src/main.cc
+++ train/src/main.cc
@@ -4,6 +4,7 @@
 
 #include <array>
 #include <cstdint>
+#include <ios>
 #include <iostream>
 #include <memory>
 #include <ranges>
@@ -12,15 +13,15 @@
 
 using namespace chess;
 
-using PgnType = std::pair<Board, std::vector<Move>>;
+using PgnType = std::vector<Move>;
 using PgnList = std::vector<PgnType>;
 
-template<typename T>
+/*template<typename T>
 std::span<T>
 make_subspan(std::span<T> span, size_t offset, size_t count) {
     count = std::min(count, span.size() > offset ? span.size() - offset : 0);
     return span.subspan(offset, count);
-}
+}*/
 
 // Convert board to input vector
 std::array<float, 64>
@@ -46,43 +47,78 @@ write_process_fen(const Process & proc, const std::string & fen) {
     write_to_process(proc.stdin_fd, "ucinewgame\n");
     write_to_process(proc.stdin_fd, "position fen " + fen + "\n");
     // write_to_process(proc.stdin_fd, "eval\n");
-    write_to_process(proc.stdin_fd, "go depth 20\n");
+    write_to_process(proc.stdin_fd, "go depth 16\n");
     write_to_process(proc.stdin_fd, "ucinewgame\n");
 }
 
 void
-write_process_uci(const Process & proc, const Board & board, std::vector<Move> & moves) {
+write_process_uci(const Process & proc, std::vector<Move> & moves) {
     // write_to_process(proc.stdin_fd, "ucinewgame\n");
     // write_to_process(proc.stdin_fd, "position fen " + fen + "\n");
     //  write_to_process(proc.stdin_fd, "eval\n");
 
     write_to_process(proc.stdin_fd, "ucinewgame\n");
-    const auto span = std::span<Move>{moves};
+    const auto span = std::span<Move>(moves.data(), moves.size());
 
-    for (auto count = 1; count <= moves.size(); count++) {
-        const auto  subspan = make_subspan<Move>(span, 0, count);
+    for (std::size_t count = 1; count <= moves.size(); count++) {
+        const auto  subspan = std::span(moves.data(), count);
         std::string subspan_moves = "";
 
         for (auto move : subspan) {
             subspan_moves += uci::moveToUci(move) + " ";
         }
 
-        write_to_process(proc.stdin_fd,
-                         "position fen " + board.getFen() + " moves " + subspan_moves + "\n");
-        write_to_process(proc.stdin_fd, "go depth 20\n");
+        // write_to_process(proc.stdin_fd,
+        //                  "position fen " + board.getFen() + " moves " + subspan_moves + "\n");
+        write_to_process(proc.stdin_fd, "position startpos moves " + subspan_moves + "\n");
+        write_to_process(proc.stdin_fd, "go depth 16\n");
     }
 
     write_to_process(proc.stdin_fd, "ucinewgame\n");
 }
 
+class MyCounter : public pgn::Visitor {
+public:
+    virtual ~MyCounter() {}
+
+    void
+    startPgn() {
+        count++;
+    }
+
+    void
+    header(std::string_view key, std::string_view value) {}
+
+    void
+    startMoves() {}
+
+    void
+    move(std::string_view move, std::string_view comment) {}
+
+    void
+    endPgn() {}
+
+    std::size_t
+    getCount() {
+        return count;
+    }
+
+private:
+    std::size_t count;
+    Board       board;
+};
+
 class MyVisitor : public pgn::Visitor {
 public:
+    MyVisitor(std::size_t count) : count_{count} {}
+
     virtual ~MyVisitor() {}
 
     void
     startPgn() {
-        board_moves = board;
-        pgns.emplace_back(board, std::vector<Move>{});
+        board_move = board;
+        moves.clear();
+        boards_move.clear();
     }
 
     void
@@ -93,40 +129,107 @@ public:
 
     void
     move(std::string_view move, std::string_view comment) {
-        auto & last = pgns.back().second;
-        auto   parsed = uci::parseSan(board_moves, move);
-        board_moves.makeMove(parsed);
-        // std::cout << boards.size() << ": " << move << std::endl;
-        boards.push_back(board_moves);
-        // std::cout << last.size() << ": " << parsed << std::endl;
-        last.push_back(parsed);
-        // std::cout << board_moves.getFen() << ": " << move << std::endl;
+        auto parsed = uci::parseSan(board_move, move);
+        moves.push_back(parsed);
+        board_move.makeMove(parsed);
+        boards_move.push_back(board_move);
     }
 
     void
-    endPgn() {}
+    endPgn() {
+        std::vector<float> evals;
 
-    PgnList &
-    getPgns() {
-        return pgns;
-    }
+        setvbuf(stdout, nullptr, _IONBF, 0);
 
-    std::vector<Board> &
-    getBoards() {
-        return boards;
+        // Run stockfish
+        auto proc = start_process(
+            "/usr/bin/stdbuf",
+            {"-o0", "/mnt/c/Users/CoolJWB/Desktop/Programming/chess/stockfish/src/stockfish"});
+        write_to_process(proc.stdin_fd, "uci\n");
+        write_to_process(proc.stdin_fd, "setoption name Threads value 32");
+        write_process_uci(proc, moves);
+
+        // Quit stockfish
+        write_to_process(proc.stdin_fd, "quit\n", true);
+        auto out = read_from_process(proc.stdout_fd);
+
+        std::stringstream ss(out);
+        std::string       prev;
+        std::string       current;
+
+        auto extract_score = [](const std::string & line) -> int {
+            std::istringstream iss(line);
+            std::string        token;
+
+            while (iss >> token) {
+                if (token == "cp") {
+                    int cp;
+                    iss >> cp;
+                    return cp;
+                } else if (token == "mate") {
+                    int mate;
+                    iss >> mate;
+                    return mate > 0 ? 10000 - mate * 100 : -10000 + mate * 100;
+                }
+            }
+            return 0;
+        };
+
+        std::ofstream file2("./eval.txt", std::ios::app);
+        bool          white = false;
+
+        while (std::getline(ss, current, '\n')) {
+            if (prev.starts_with("info") && current.starts_with("bestmove")) {
+                auto cp = extract_score(prev);
+                auto score = cp / 100.0f;
+
+                file2 << boards_move.at(evals.size()).getFen() << " ; " << (white ? cp : -cp)
+                      << '\n';
+                evals.push_back(white ? score : -score);
+                white = !white;
+            }
+
+            prev = current;
+        }
+
+        file2.close();
+
+        std::vector<std::array<float, 64>> positions;
+        // std::vector<float>                 evals; // e.g., 0.25 for +0.25 pawns
+        minimal_mlp::MLP net; // default: 64 → 32 → 1
+
+        for (const auto & board : boards_move) {
+            positions.push_back(encode_board(board));
+        }
+
+        net.load("network.bin");
+        net.train(positions, evals, 16, 0.01f);
+        net.save("network.bin");
+
+        done_++;
+
+        std::cout << "Progress: " << ((done_ * 100.0f) / count_) << "%" << std::endl;
+
+        // float eval = net.forward(positions.back());
+        // std::cout << "Eval: " << eval << " ; "
+        //           << "Expected: " << evals.back() << " ; "
+        //           << "Delta: " << std::abs(eval - evals.back());
     }
 
 private:
-    Board board;
-    Board board_moves;
+    std::size_t count_;
+    std::size_t done_;
+
+    Board              board;
+    Board              board_move;
+    std::vector<Move>  moves;
+    std::vector<Board> boards_move;
+
     // const Board board_default =
     //     Board::fromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    static PgnList            pgns;
-    static std::vector<Board> boards;
+    // static PgnList            pgns;
+    // static std::vector<Board> boards;
 };
-
-PgnList            MyVisitor::pgns;
-std::vector<Board> MyVisitor::boards;
 
 int
 main() {
@@ -256,6 +359,7 @@ main() {
 
     std::cout << pgns.front();*/
 
+    /*
     // Steam the PGN file
     auto file_stream = std::ifstream("./lichess_db_standard_rated_2016-03.pgn");
     // auto file_stream = std::ifstream("./my.pgn");
@@ -287,8 +391,8 @@ main() {
         write_to_process(proc.stdin_fd, "uci\n");
         write_to_process(proc.stdin_fd, "setoption name Threads value 16");
 
-        for (auto & pgn : subspan) {
-            write_process_uci(proc, pgn.first, pgn.second);
+        for (auto & moves : subspan) {
+            write_process_uci(proc, moves);
         }
 
         // Quit stockfish
@@ -351,5 +455,33 @@ main() {
     net.save("network.bin");
 
     float eval = net.forward(positions.front());
-    std::cout << "Eval = " << eval << "\n";
+    std::cout << "Eval = " << eval << "\n";*/
+
+    // Steam the PGN file
+    auto file_stream = std::ifstream("./lichess_db_standard_rated_2013-01.pgn");
+    // auto file_stream = std::ifstream("./my.pgn");
+    // auto file_stream = std::ifstream("./broke.pgn");
+    auto cnt = std::make_unique<MyCounter>();
+
+    pgn::StreamParser parser_cnt(file_stream);
+    auto              error = parser_cnt.readGames(*cnt);
+
+    if (error) {
+        std::cerr << "Error counter: " << error.message() << "\n";
+        return 1;
+    }
+
+    const auto count = cnt.get()->getCount();
+    std::cout << "Total count: " << count << "\n";
+    auto vis = std::make_unique<MyVisitor>(count);
+
+    file_stream.clear();
+    file_stream.seekg(0, std::ios::beg);
+    pgn::StreamParser parser_vis(file_stream);
+    error = parser_vis.readGames(*vis);
+
+    if (error) {
+        std::cerr << "Error visitor: " << error.message() << "\n";
+        return 1;
+    }
 }
